3e8ece014643b611bb25e3148044ef44ae284d9f
NurHossain9 <nur.hossain@durbar.live>
2024-01-01 12:56:07 +0600

    add logo

--- app/src/main/java/com/example/sangsadprotidin/util.kt
+++ app/src/main/java/com/example/sangsadprotidin/util.kt
@@ -1,14 +1,6 @@
 package com.example.sangsadprotidin
 
-import android.content.Context
-import android.content.DialogInterface
-import android.content.Intent
-import android.net.Uri
-import android.provider.Settings
 import android.view.View
-import android.widget.Toast
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
-
 enum class Status{
     Available,Unavailable
 }
