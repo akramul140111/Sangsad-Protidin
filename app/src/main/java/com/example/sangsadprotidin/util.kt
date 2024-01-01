ca91ba9c63fe262ac3f9411ab78a583c6547395e
NurHossain9 <nur.hossain@durbar.live>
2024-01-01 12:47:49 +0600

    updated web view sangsad protidin app

--- app/src/main/java/com/example/sangsadprotidin/util.kt
+++ app/src/main/java/com/example/sangsadprotidin/util.kt
@@ -21,24 +21,3 @@ fun visible(view : View){
     view.visibility = View.VISIBLE
 }
 
-
-fun appSettingOpen(context: Context){
-    Toast.makeText(
-        context,
-        "Go to Setting and Enable All Permission",
-        Toast.LENGTH_LONG
-    ).show()
-
-    val settingIntent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-    settingIntent.data = Uri.parse("package:${context.packageName}")
-    context.startActivity(settingIntent)
-}
-
-fun warningPermissionDialog(context: Context,listener : DialogInterface.OnClickListener){
-    MaterialAlertDialogBuilder(context)
-        .setMessage("All Permission are Required for this app")
-        .setCancelable(false)
-        .setPositiveButton("Ok",listener)
-        .create()
-        .show()
-}
